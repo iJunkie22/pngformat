5f3ec402a11aa74bd0d2046d7979ecc3e6aa6353
Ethan Randall <ethan@rabbit5.local>
2016-07-08 14:17:16 -0400

    added IHDR specific handles

--- pngformat.py
+++ pngformat.py
@@ -5,6 +5,7 @@ import binascii
 import base64
 import zlib
 import io
+from collections import OrderedDict
 
 
 PNG_HEAD = b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'
@@ -17,68 +18,6 @@ PNG_IEND = 'IEND'
 bstr = binascii.hexlify
 
 TEST_FP = 'pngImages/test1.png'
-
-
-class PngFileHandle(object):
-    def __init__(self, *args, **kwargs):
-        self.chunks = []
-
-    @property
-    def width(self):
-        ihdr_block = None
-        for ch2 in self.chunks:
-            if ch2.chunk_type == PNG_IHDR:
-                ihdr_block = ch2
-
-        width_bytes = ihdr_block.chunk_data[:4]
-        return struct.unpack('>i', width_bytes)[0]
-
-
-
-    @property
-    def decompressed_data(self):
-        concated_chunks = b''
-        for ch1 in self.chunks:
-            if ch1.chunk_type == PNG_IDAT:
-                concated_chunks += ch1.chunk_data
-
-        return zlib.decompress(concated_chunks, 0)
-
-    @property
-    def dec_data_as_hex(self):
-        return binascii.hexlify(self.decompressed_data)
-
-    @classmethod
-    def read_file(cls, fp):
-        new_png = cls()
-
-        fd1 = open(fp, 'rb')
-
-        try:
-            header = fd1.read(len(PNG_HEAD))
-            last_chunk = PngChunk()
-            last_chunk.chunk_type = 'XXXX'
-            while last_chunk.chunk_type != PNG_IEND:
-                last_chunk = PngChunk.read_new(fd1)
-                new_png.chunks.append(last_chunk)
-        finally:
-            fd1.close()
-        return new_png
-
-    def get_pixels(self):
-        width = self.width
-        pix_data = self.decompressed_data
-        data_buf = io.BytesIO(pix_data)
-        pix_len = len(pix_data)
-        lines_pixls = 0
-        while data_buf.tell() < pix_len:
-            if lines_pixls % width == 0:
-                line_bit = data_buf.read(1)
-            pix_bin = data_buf.read(4)
-            lines_pixls += 1
-            yield data_buf.tell(), bstr(pix_bin), struct.unpack('>4B', pix_bin)
-
-        data_buf.close()
 
 
 class PngChunk(object):
@@ -131,6 +70,129 @@ class PngChunk(object):
         return binascii.hexlify(self.chunk_data)
 
 
+class PngFileHandle(object):
+    def __init__(self, *args, **kwargs):
+        self.chunks = []
+
+    @property
+    def ihdr_block(self):
+        assert len(self.chunks) > 0, 'No chunks found'
+        chunk_x = self.chunks[0]
+        assert isinstance(chunk_x, PngChunk)
+        assert chunk_x.chunk_type == PNG_IHDR, 'Incorrect type code'
+        return chunk_x
+
+    @property
+    def ihdr_dict(self):
+        return IHDRDict.frombytes(self.ihdr_block.chunk_data)
+
+    @property
+    def width(self):
+        return self.ihdr_dict.width
+
+    @property
+    def decompressed_data(self):
+        concated_chunks = b''
+        for ch1 in self.chunks:
+            if ch1.chunk_type == PNG_IDAT:
+                concated_chunks += ch1.chunk_data
+
+        return zlib.decompress(concated_chunks, 0)
+
+    @property
+    def dec_data_as_hex(self):
+        return binascii.hexlify(self.decompressed_data)
+
+    @classmethod
+    def read_file(cls, fp):
+        new_png = cls()
+
+        fd1 = open(fp, 'rb')
+
+        try:
+            header = fd1.read(len(PNG_HEAD))
+            last_chunk = PngChunk()
+            last_chunk.chunk_type = 'XXXX'
+            while last_chunk.chunk_type != PNG_IEND:
+                last_chunk = PngChunk.read_new(fd1)
+                new_png.chunks.append(last_chunk)
+        finally:
+            fd1.close()
+        return new_png
+
+    def get_pixels(self):
+        width = self.width
+        pix_data = self.decompressed_data
+        data_buf = io.BytesIO(pix_data)
+        pix_len = len(pix_data)
+        lines_pixls = 0
+        while data_buf.tell() < pix_len:
+            if lines_pixls % width == 0:
+                line_bit = data_buf.read(1)
+            pix_bin = data_buf.read(4)
+            lines_pixls += 1
+            yield data_buf.tell(), bstr(pix_bin), struct.unpack('>4B', pix_bin)
+
+        data_buf.close()
+
+
+class IHDRDict(OrderedDict, object):
+    CHUNK_KEYS = ('Width', 'Height', 'Bit depth', 'Color type', 'Compression method',
+                  'Filter method', 'Interlace method')
+
+    @classmethod
+    def frombytes(cls, bs):
+        if len(bs) == 25:
+            sformat = '>8x2i5b4x'
+        elif len(bs) == 13:
+            sformat = '>2i5b'
+        else:
+            raise RuntimeError('Invalid IHDR length')
+
+        chunk_values = struct.unpack(sformat, bs)
+        return cls(zip(cls.CHUNK_KEYS, chunk_values))
+
+    @property
+    def width(self):
+        return self['Width']
+
+    @property
+    def height(self):
+        return self['Height']
+
+    @property
+    def bit_depth(self):
+        return self['Bit depth']
+
+    @property
+    def color_type(self):
+        return self['Color type']
+
+    @property
+    def compression_method(self):
+        return self['Compression method']
+
+    @property
+    def filter_method(self):
+        return self['Filter method']
+
+    @property
+    def interlace_method(self):
+        return self['Interlace method']
+
+    @property
+    def data_bytes(self):
+        return struct.pack('>2i5b', *self.values())
+
+    @property
+    def crc(self):
+        return zlib.crc32(struct.pack('>4s', 'IHDR') + self.data_bytes)
+
+    @property
+    def chunk_bytes(self):
+        return struct.pack('>i4s', 25, 'IHDR') + self.data_bytes + struct.pack('>i', self.crc)
+
+
 def read_chunk(fd_in):
     cheader = fd_in.read(8)
     if len(cheader) != 8:
@@ -146,6 +208,9 @@ png1 = PngFileHandle.read_file(TEST_FP)
 for ch in png1.chunks:
     if ch.chunk_type == PNG_IDAT:
         print ch.data_as_hex[:30]
+
+print bstr(png1.ihdr_dict.chunk_bytes)
+print png1.ihdr_dict.crc
 
 with open('dump.txt', 'w') as fd2:
     for p1 in png1.get_pixels():
